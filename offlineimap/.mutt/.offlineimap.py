29000150ca43ef8b7d54d63bd8db85697be6071d
Hussein Mohammed <zidhussein@gmail.com>
2017-07-01 21:45:36 +0100

    Borderless & gapless

--- offlineimap/.mutt/.offlineimap.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-from subprocess import check_output
-
-def get_pass(account):
-    return check_output("pass mail/"+account+" | head -1", shell=True).strip()
