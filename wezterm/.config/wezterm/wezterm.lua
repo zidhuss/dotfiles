4b1b4b6d363bf2a17d0e4f128263980962491a90
Hussein Al Abry <hussein@zidhuss.tech>
2022-05-16 16:09:09 +0400

    wexterm: fix for option/alt key on wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font("FiraCode Nerd Font"),
+    font = wezterm.font("FiraCode Nerd Font"),
+    send_composed_key_when_left_alt_is_pressed = true
 }
