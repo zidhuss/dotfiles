aa9e834e20b70c9661dbed43524911775328b930
Hussein Al Abry <hussein@zidhuss.tech>
2022-06-08 14:26:16 +0400

    wezterm: switch terminal theme when OS theme changes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,25 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "VibrantInk"
+	else
+		return "primary"
+	end
+end
+
+wezterm.on("window-config-reloaded", function(window, pane)
+	local overrides = window:get_config_overrides() or {}
+	local appearance = window:get_appearance()
+	local scheme = scheme_for_appearance(appearance)
+	if overrides.color_scheme ~= scheme then
+		overrides.color_scheme = scheme
+		window:set_config_overrides(overrides)
+	end
+end)
+
 return {
-    font = wezterm.font("FiraCode Nerd Font"),
-    send_composed_key_when_left_alt_is_pressed = true
+	font = wezterm.font("FiraCode Nerd Font"),
+	send_composed_key_when_left_alt_is_pressed = true,
+	term = "wezterm",
 }
