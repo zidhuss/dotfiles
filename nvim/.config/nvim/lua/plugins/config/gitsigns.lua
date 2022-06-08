fe7104a4e4c322cb73cc84dc118aca5cbdf69c85
Hussein Al Abry <hussein@zidhuss.tech>
2022-06-08 14:25:43 +0400

    nvim: mapping to move between git hunks

--- nvim/.config/nvim/lua/plugins/config/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/config/gitsigns.lua
@@ -1,3 +1,35 @@
 local gitsigns = require("gitsigns")
 
-gitsigns.setup({})
+gitsigns.setup({
+
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		-- Navigation
+		map("n", "]c", function()
+			if vim.wo.diff then
+				return "]c"
+			end
+			vim.schedule(function()
+				gs.next_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		map("n", "[c", function()
+			if vim.wo.diff then
+				return "[c"
+			end
+			vim.schedule(function()
+				gs.prev_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+	end,
+})
