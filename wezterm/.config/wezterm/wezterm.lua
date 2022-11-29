d35fe017dbdd1c1968e7f4810099c25aed194b5e
Hussein Al Abry <hussein@zidhuss.tech>
2022-11-29 22:42:31 +0400

    wezterm: Fura font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -19,7 +19,8 @@ wezterm.on("window-config-reloaded", function(window, pane)
 end)
 
 return {
-	font = wezterm.font("FiraCode Nerd Font"),
+	font = wezterm.font("FuraCode Nerd Font"),
+	hide_tab_bar_if_only_one_tab = true,
 	send_composed_key_when_left_alt_is_pressed = true,
 	term = "wezterm",
 }
