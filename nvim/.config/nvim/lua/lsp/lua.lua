d72f57e373a818951dce4f7457a94240a8bdfaaf
Hussein Al Abry <hussein@zidhuss.tech>
2023-02-19 16:35:11 +0400

    nvim: rename to lua_ls

--- nvim/.config/nvim/lua/lsp/lua.lua
+++ nvim/.config/nvim/lua/lsp/lua.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.setup = function(on_attach, capabilities)
-	require("lspconfig").sumneko_lua.setup({
+	require("lspconfig").lua_ls.setup({
 		on_attach = on_attach,
 		settings = {
 			Lua = {
