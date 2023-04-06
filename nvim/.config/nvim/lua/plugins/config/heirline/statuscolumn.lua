f31451f95c3270989283efb1495e3995d55bc236
Hussein Al Abry <hussein@zidhuss.tech>
2023-04-06 22:50:41 +0400

    nvim: simplify status column line number function

--- nvim/.config/nvim/lua/plugins/config/heirline/statuscolumn.lua
+++ nvim/.config/nvim/lua/plugins/config/heirline/statuscolumn.lua
@@ -1,6 +1,14 @@
 local shared = require("plugins.config.heirline.shared")
 
-local LineNumbers = { provider = "%=%4{v:wrap ? '' : &nu ? (&rnu && v:relnum ? v:relnum : v:lnum) : ''}" }
+local LineNumbers = {
+	provider = function()
+		if vim.v.virtnum ~= 0 then
+			return ""
+		end
+
+		return vim.v.lnum
+	end,
+}
 
 local Signs = { provider = "%s" }
 
@@ -8,6 +16,7 @@ local Folds = { provider = "%C" }
 
 local statuscolumn = {
 	LineNumbers,
+	shared.Align,
 	shared.Space,
 	Signs,
 	Folds,
