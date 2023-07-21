cf991fb3590e16fceb99cc7b592b921775d2b6b8
Hussein Al Abry <hussein@zidhuss.tech>
2023-07-21 20:13:50 +0400

    nvim: remove copilot defer

--- nvim/.config/nvim/lua/plugins/config/copilot.lua
+++ nvim/.config/nvim/lua/plugins/config/copilot.lua
@@ -1,12 +1,10 @@
-vim.defer_fn(function()
-	require("copilot").setup({
-		filetypes = {
-			markdown = true,
-			gitcommit = true,
-			gitrebase = true,
-		},
-		method = "getCompletionsCycling",
-		suggestion = { enabled = false },
-		panel = { enabled = false },
-	})
-end, 100)
+require("copilot").setup({
+	filetypes = {
+		markdown = true,
+		gitcommit = true,
+		gitrebase = true,
+	},
+	method = "getCompletionsCycling",
+	suggestion = { enabled = false },
+	panel = { enabled = false },
+})
