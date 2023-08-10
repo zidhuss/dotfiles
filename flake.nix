cabdeda0ce896a16ea12a185e2c0a0d80b6b5ba0
Hussein Al Abry <hussein@zidhuss.tech>
2023-08-10 16:44:26 +0400

    nix: add home manager
    Right now this is only for my macbook. The packages here are some that
    I'd normally compile myself or install with homebrew.

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Home Manager configuration of abry";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."abry" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
