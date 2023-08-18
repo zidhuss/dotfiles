cf0ee8f8fc061d352857938a44710dd136345e32
Hussein Al Abry <hussein@zidhuss.tech>
2023-08-18 19:22:43 +0400

    nvim: quick save from insert mode fix

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,7 @@ vim.api.nvim_set_keymap("n", "<Space>", "<NOP>", { noremap = true, silent = true
 -- faster save and quit
 vim.keymap.set("n", "<c-s>", ":w<cr>", { desc = "save" })
 vim.keymap.set("n", "<c-q>", ":q<cr>", { desc = "quit" })
-vim.keymap.set("i", "<c-s>", ":w<cr>", { desc = "save" })
+vim.keymap.set("i", "<c-s>", "<esc>:w<cr>", { desc = "save" })
 
 -- remove highlight
 vim.keymap.set("n", "<bs>", "<cmd>nohlsearch<cr>", { desc = "remove search highlights" })
