b2f4faa809d9d9c03e3ad516b3e53e2420a13fa2
Hussein Al Abry <hussein@zidhuss.tech>
2023-10-31 15:02:42 +0400

    nvim: update dotnet lsp
    OmniSharp will be provided by project nix flake.

--- nvim/.config/nvim/lua/lsp/dotnet.lua
+++ nvim/.config/nvim/lua/lsp/dotnet.lua
@@ -1,13 +1,10 @@
 local M = {}
 
-local pid = vim.fn.getpid()
-local omnisharp_bin = "/usr/bin/omnisharp"
-
 M.setup = function(on_attach, capabilities)
 	require("lspconfig").omnisharp.setup({
 		on_attach = on_attach,
 		capabilities = capabilities,
-		cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
+		cmd = { "OmniSharp" },
 	})
 end
 
