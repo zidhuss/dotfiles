30e43881856b67c04a4dd07b05cef0e9bbc3870f
Hussein Al Abry <hussein@zidhuss.tech>
2023-11-01 22:28:46 +0400

    nvim: recognize k8s.yaml as kubernetes schema

--- nvim/.config/nvim/lua/lsp/yaml.lua
+++ nvim/.config/nvim/lua/lsp/yaml.lua
@@ -3,7 +3,7 @@ local M = {}
 M.setup = function(on_attach, capabilities)
 	require("lspconfig").yamlls.setup({
 		on_attach = on_attach,
-		settings = { yaml = { schemas = { kubernetes = "k8s/*" }, format = { enable = false } } },
+		settings = { yaml = { schemas = { kubernetes = { "k8s/*", "k8s.yaml" } }, format = { enable = false } } },
 		capabilities = capabilities,
 	})
 end
