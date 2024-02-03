2a7c0f5c5e07c07fd46053dba2fd881af71b7aea
Hussein Al Abry <hussein@zidhuss.tech>
2024-02-03 11:16:03 +0400

    nix: direnv added as a package
    I'm also using nix-direnv but installing that separately. I'll install both in
    the recommended home-manager way in a future commit.

--- home.nix
+++ home.nix
@@ -40,6 +40,8 @@
 
     alejandra
 
+    direnv
+
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
