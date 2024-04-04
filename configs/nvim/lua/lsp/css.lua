4690ee28ff0b54035c545c4627d5ae7bd43b87ed
Hussein Al Abry <hussein@zidhuss.tech>
2024-04-04 11:14:48 +0400

    nvim: configure tailwind language server

--- configs/nvim/lua/lsp/css.lua
+++ configs/nvim/lua/lsp/css.lua
@@ -2,6 +2,8 @@ local M = {}
 
 M.setup = function(on_attach, capabilities)
 	require("lspconfig").cssls.setup({ on_attach = on_attach, capabilities = capabilities })
+
+	require("lspconfig").tailwindcss.setup({ on_attach = on_attach, capabilities = capabilities })
 end
 
 return M
