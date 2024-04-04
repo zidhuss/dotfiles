664abe6741c8cae09ab16199a61d34cae18b1b1c
Hussein Al Abry <hussein@zidhuss.tech>
2024-04-04 15:34:38 +0400

    nvim: remove deprecated call

--- configs/nvim/lua/lsp/callbacks.lua
+++ configs/nvim/lua/lsp/callbacks.lua
@@ -5,11 +5,8 @@ M.on_attach = function(client, bufnr)
 	local function buf_set_keymap(...)
 		vim.api.nvim_buf_set_keymap(bufnr, ...)
 	end
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
 
-	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+	vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
 
 	-- Mappings.
 	local opts = { noremap = true, silent = true }
