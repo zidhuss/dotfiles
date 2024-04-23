120bde35d94365071a99dc635f3791e5dcc93a8e
Hussein Al Abry <hussein@zidhuss.tech>
2024-04-23 16:53:16 +0400

    nvim: move firenvim config

---
+++ configs/nvim/lua/plugins/firenvim.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"glacambre/firenvim",
+		lazy = not vim.g.started_by_firenvim,
+		build = function()
+			vim.fn["firenvim#install"](0)
+		end,
+		config = function()
+			if vim.g.started_by_firenvim then
+				vim.o.laststatus = 0
+				vim.o.guifont = "FuraCode Nerd Font:h18"
+
+				vim.api.nvim_create_autocmd(
+					"BufEnter",
+					{ pattern = { "github.com_*.txt" }, command = "set filetype=markdown" }
+				)
+			end
+
+			vim.g.firenvim_config = {
+				globalSettings = { alt = "all" },
+				localSettings = {
+					[".*"] = {
+						cmdline = "neovim",
+						content = "text",
+						priority = 0,
+						selector = "textarea",
+						takeover = "never",
+					},
+					["https?://mail.google.com/"] = { takeover = "never", priority = 1 },
+					["https?://discord.com/"] = { takeover = "never", priority = 1 },
+					["https?://github.com/"] = {
+						takeover = "always",
+						selector = "textarea:not([id='read-only-cursor-text-area'])",
+						priority = 1,
+					},
+				},
+			}
+		end,
+	},
+}
--- configs/nvim/lua/plugins/main.lua
+++ configs/nvim/lua/plugins/main.lua
@@ -5,22 +5,6 @@ return {
 		config = true,
 	},
 
-	-- firefox integration
-	{
-		"glacambre/firenvim",
-
-		-- Lazy load firenvim
-		-- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-		cond = not not vim.g.started_by_firenvim,
-		build = function()
-			require("lazy").load({ plugins = "firenvim", wait = true })
-			vim.fn["firenvim#install"](0)
-		end,
-		config = function()
-			require("plugins.config.firenvim")
-		end,
-	},
-
 	-- LSP plugins
 	{
 		"neovim/nvim-lspconfig",
