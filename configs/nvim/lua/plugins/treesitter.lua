d50beefdbb60f97ba148e296f5d313f20b097a2f
Hussein Al Abry <hussein@zidhuss.tech>
2024-05-23 15:39:54 +0400

    nvim: move treesitter config

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -11,7 +11,62 @@ return {
 		},
 
 		config = function()
-			require("plugins.config.treesitter")
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = "all",
+				ignore_install = { "phpdoc", "wing" },
+				highlight = { enable = true },
+				indent = { enable = true },
+				endwise = { enable = true },
+				matchup = { enable = true },
+
+				textobjects = {
+					select = {
+						enable = true,
+
+						-- Automatically jump forward to textobj, similar to targets.vim
+						lookahead = true,
+
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]]"] = "@class.outer",
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
+					},
+				},
+			})
 		end,
 	},
 
