970a6ce5437b21e2c851a67c9f3ad07df41ea163
Hussein Al Abry <hussein@zidhuss.tech>
2024-05-30 09:05:36 +0400

    nvim: commands to enable/disable format on save

--- configs/nvim/lua/plugins/formatting.lua
+++ configs/nvim/lua/plugins/formatting.lua
@@ -4,10 +4,18 @@ return {
 		event = { "BufWritePre" },
 		cmd = { "ConformInfo" },
 		opts = {
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+
+				return {
+					timeout_ms = 500,
+					lsp_fallback = true,
+				}
+			end,
+
 			formatters_by_ft = {
 				python = { "ruff_fix", "ruff_format" },
 
@@ -47,5 +55,41 @@ return {
 				["_"] = { "trim_newlines", "trim_whitespace" },
 			},
 		},
+
+		config = function(_, opts)
+			require("conform").setup(opts)
+
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+					vim.notify("Autoformat disabled for this buffer", "info", { title = "Conform" })
+				else
+					vim.g.disable_autoformat = true
+					vim.notify("Autoformat disabled", "info", { title = "Conform" })
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+				vim.notify("Autoformat enabled", "info", { title = "Conform" })
+			end, {
+				desc = "Re-enable autoformat-on-save",
+			})
+
+			vim.api.nvim_create_user_command("FormatToggle", function()
+				if vim.b.disable_autoformat or vim.g.disable_autoformat then
+					vim.cmd("FormatEnable")
+				else
+					vim.cmd("FormatDisable")
+				end
+			end, {
+				desc = "Toggle autoformat-on-save",
+			})
+		end,
 	},
 }
