c59f3ce246deb1737328fad93056c67dee30a970
Hussein Al Abry <hussein@zidhuss.tech>
2024-07-16 21:44:19 +0400

    nvim: add nio for neotest

--- configs/nvim/lua/plugins/test.lua
+++ configs/nvim/lua/plugins/test.lua
@@ -4,6 +4,7 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
+			"nvim-neotest/nvim-nio",
 			"nvim-neotest/neotest-python",
 			"vim-test/vim-test",
 			"antoinemadec/FixCursorHold.nvim",
