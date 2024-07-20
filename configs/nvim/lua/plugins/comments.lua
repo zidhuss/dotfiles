3b0b27c64b226ec9ca1a9043572524ff001098f3
Hussein Al Abry <hussein@zidhuss.tech>
2024-07-20 14:34:38 +0400

    nvim: make sure comments still work for jsx

--- configs/nvim/lua/plugins/comments.lua
+++ configs/nvim/lua/plugins/comments.lua
@@ -1,6 +1,19 @@
 return {
-	{ "numToStr/Comment.nvim", opts = {} },
-	"JoosepAlviste/nvim-ts-context-commentstring",
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+
+			pre_hook = function(...)
+				return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(...)
+			end,
+		},
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			opts = {
+				enable_autocmd = false,
+			},
+		},
+	},
 	{
 		"folke/todo-comments.nvim",
 		dependencies = "nvim-lua/plenary.nvim",
