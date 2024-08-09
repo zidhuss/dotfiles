1b38411deb6478963e47269f32063191ecb1fbb1
Hussein Al Abry <hussein@zidhuss.tech>
2024-08-09 11:02:29 +0400

    nvim: update firenvim build call

--- configs/nvim/lua/plugins/firenvim.lua
+++ configs/nvim/lua/plugins/firenvim.lua
@@ -2,9 +2,7 @@ return {
 	{
 		"glacambre/firenvim",
 		lazy = not vim.g.started_by_firenvim,
-		build = function()
-			vim.fn["firenvim#install"](0)
-		end,
+		build = ":call firenvim#install(0)",
 		config = function()
 			if vim.g.started_by_firenvim then
 				vim.o.laststatus = 0
