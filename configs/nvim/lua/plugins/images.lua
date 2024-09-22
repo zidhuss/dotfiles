ec6ad941e42d01f3d7f242d6b928d740fb13a570
Hussein Al Abry <hussein@zidhuss.tech>
2024-09-22 16:56:44 +0400

    nvim: add img-clip.nvim
    Useful for pasting images from the clipboard.

---
+++ configs/nvim/lua/plugins/images.lua
@@ -0,0 +1,9 @@
+return {
+	"HakonHarnes/img-clip.nvim",
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		-- suggested keymap
+		{ "<leader>P", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+	},
+}
