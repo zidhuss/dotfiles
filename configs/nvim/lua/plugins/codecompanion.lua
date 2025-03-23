1974c9d0fc6b4a640cc0c29617f5066609184ebb
Hussein Al Abry <hussein@zidhuss.tech>
2025-03-23 11:52:04 +0400

    nvim: replace copilot-chat with codecompanion

---
+++ configs/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,20 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {
+		adapters = {
+			copilot = function()
+				return require("codecompanion.adapters").extend("copilot", {
+					schema = {
+						model = {
+							default = "claude-3.7-sonnet",
+						},
+					},
+				})
+			end,
+		},
+	},
+}
