6847443f3902c3ad305204e09b43b4d6069c610f
Hussein Al Abry <hussein@zidhuss.tech>
2025-06-22 11:25:12 +0400

    nvim: update jdtls config

--- configs/nvim/lua/plugins/jdtls.lua
+++ configs/nvim/lua/plugins/jdtls.lua
@@ -1,6 +1,3 @@
-local callbacks = require("lsp.callbacks")
-local capabilities = callbacks.capabilities
-local on_attach = callbacks.on_attach
 local java_filetypes = { "java" }
 return {
 	{
@@ -16,7 +13,6 @@ return {
 				cmd = {
 					vim.fn.exepath("jdtls"),
 				},
-				capatibilities = capabilities,
 			}
 		end,
 		config = function(_, opts)
@@ -36,9 +32,42 @@ return {
 			vim.api.nvim_create_autocmd("LspAttach", {
 				callback = function(args)
 					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					local wk = require("which-key")
 					if client and client.name == "jdtls" then
-
-						on_attach(client, args.buf)
+						wk.add({
+							{
+								mode = "n",
+								buffer = args.buf,
+								{ "<leader>cx", group = "extract" },
+								{ "<leader>cxv", require("jdtls").extract_variable_all, desc = "Extract Variable" },
+								{ "<leader>cxc", require("jdtls").extract_constant, desc = "Extract Constant" },
+								{ "<leader>cgs", require("jdtls").super_implementation, desc = "Goto Super" },
+								{ "<leader>cgS", require("jdtls.tests").goto_subjects, desc = "Goto Subjects" },
+								{ "<leader>co", require("jdtls").organize_imports, desc = "Organize Imports" },
+							},
+						})
+						wk.add({
+							{
+								mode = "v",
+								buffer = args.buf,
+								{ "<leader>cx", group = "extract" },
+								{
+									"<leader>cxm",
+									[[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]],
+									desc = "Extract Method",
+								},
+								{
+									"<leader>cxv",
+									[[<ESC><CMD>lua require('jdtls').extract_variable_all(true)<CR>]],
+									desc = "Extract Variable",
+								},
+								{
+									"<leader>cxc",
+									[[<ESC><CMD>lua require('jdtls').extract_constant(true)<CR>]],
+									desc = "Extract Constant",
+								},
+							},
+						})
 					end
 				end,
 			})
