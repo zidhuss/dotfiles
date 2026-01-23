40a78ce5cf711a7b392829a5eb0c57922a24d72b
Hussein Al Abry <hussein@zidhuss.tech>
2026-01-23 14:03:32 +0400

    nix: update nix-darwin repo

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:nix-darwin/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
