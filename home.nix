7c37d23a4d49cd78f744cec3302956a867bb017b
Hussein Al Abry <hussein@zidhuss.tech>
2026-05-12 11:33:14 +0400

    nix: prefer xdg dirs

--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@
 in {
   home.username = "abry";
   home.homeDirectory = "/Users/abry";
+  home.preferXdgDirectories = true;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
